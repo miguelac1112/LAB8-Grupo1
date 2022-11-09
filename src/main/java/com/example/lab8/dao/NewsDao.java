4c2feaca08b3275e3c20e43e000668c945bd12ea
Miguel <a20190050@pucp.edu.pe>
2022-11-08 21:21:29 -0500

    lista de noticias?

--- src/main/java/com/example/lab8/dao/NewsDao.java
+++ src/main/java/com/example/lab8/dao/NewsDao.java
@@ -1,4 +1,29 @@
 package com.example.lab8.dao;
 
+import com.example.lab8.entity.News;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
 public class NewsDao {
+
+    public List<News> listarNoticias() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<News[]> responseEntity = restTemplate.getForEntity(
+                "https://api.coinstats.app/public/v1/news", News[].class);
+
+        if(responseEntity.getStatusCode() == HttpStatus.OK){
+            News[] productsList = responseEntity.getBody();
+            return Arrays.asList(productsList);
+        }else{
+            return new ArrayList<>();
+        }
+
+    }
 }
