b6c0de57e527d2aba93fddd4cdd00a12bb2225eb
FrankLaquise <a20175947@pucp.edu.pe>
2022-11-08 21:39:28 -0500

    prime modificacion

--- src/main/java/com/example/lab8/controller/CoinsController.java
+++ src/main/java/com/example/lab8/controller/CoinsController.java
@@ -1,5 +1,19 @@
 package com.example.lab8.controller;
 
+import com.example.lab8.entity.Coins;
+import com.example.lab8.service.impl.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import com.example.lab8.Dao.CoinsDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +25,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/coins")
 public class CoinsController {
+    @Autowired
+    private Service service;
+    @GetMapping(value = {"/list",""})
+    public String findAll(@RequestParam(name="buscarx" , required = false) String buscarx, @RequestParam Map<String,Object> params, Model model){
+        int page = params.get("page") != null ?(Integer.valueOf(params.get("page").toString())-1):0;
+
+        if (buscarx != null){
+            Page<Coins> pageIncidencia = service.findProductsWithPaginationAndSorting(page,10,buscarx);
+            int totalPage  = pageIncidencia.getTotalPages();
+
+            if (totalPage>0){
+                List<Integer> pages  = IntStream.rangeClosed(1,totalPage).boxed().collect(Collectors.toList());
+                model.addAttribute("pages",pages);
+            }
+            model.addAttribute("lista",pageIncidencia.getContent());
+            model.addAttribute("current",page+1);
+            model.addAttribute("next",page+2);
+            model.addAttribute("prev",page);
+            model.addAttribute("last",totalPage);
+
+            model.addAttribute("ordenarpor",buscarx);
+        }else {
+            Page<Coins> pageIncidencia = service.findProductsWithPaginationAndSorting(page,6,"titulo");
+            int totalPage  = pageIncidencia.getTotalPages();
+
+            if (totalPage>0){
+                List<Integer> pages  = IntStream.rangeClosed(1,totalPage).boxed().collect(Collectors.toList());
+                model.addAttribute("pages",pages);
+            }
+            model.addAttribute("lista",pageIncidencia.getContent());
+            model.addAttribute("current",page+1);
+            model.addAttribute("next",page+2);
+            model.addAttribute("prev",page);
+            model.addAttribute("last",totalPage);
+
+            model.addAttribute("ordenarpor","horaCreacion");
+        }
+
+
+        return "Usuario_ListaIncidencias";
+    }
+
+
     @Autowired
     CoinsDao coinsDao;
 
--- src/main/java/com/example/lab8/repository/CoinsRepository.java
+++ src/main/java/com/example/lab8/repository/CoinsRepository.java
@@ -1,4 +1,10 @@
 package com.example.lab8.repository;
 
-public interface CoinsRepository {
+import com.example.lab8.entity.Coins;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoinsRepository extends JpaRepository<Coins,String>{
+
+
+
 }
